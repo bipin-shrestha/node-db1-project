ba81e40e870c09886ed26059516af7bfbf10ea7c
bipin-shrestha <iambpn.shrestha@gmail.com>
2021-03-28 10:18:21 -0700

    progress

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,11 +1,58 @@
+const accounts = require('./accounts-model.js')
+
 exports.checkAccountPayload = (req, res, next) => {
   // DO YOUR MAGIC
+  if(req.body){
+    if(req.body.name !== undefined || req.body.budget  !== undefined ){
+      if(typeof req.body.name == 'string'){
+        if(req.body.name.trim().length < 3 || req.body.name.length > 100 ){
+          next ({ message: "Name of accont must be between 3 and 100", status: 500 })
+        } else {
+          if(typeof req.body.budget !== 'number'){
+            next({ message: "budget of account must be a number", status: 500 })
+          } else {
+            if(req.body.budget < 0 || req.body.bdget > 1000000 ){
+              next({ message: "budget of account is too large or too small", status : 500 })
+            } else {
+              next();
+            }
+          }
+        }
+      } else {
+        next({ message: "Name of the Account must be String", status: 500 })
+      }
+    } else {
+      next({ message: "Name and Budget are Required", status: 500 })
+    }
+
+  } else {
+    next({ message: "Request Body is Invalid", status: 400 })
+  }
 }
 
 exports.checkAccountNameUnique = async (req, res, next) => {
-  // DO YOUR MAGIC
+  // DO YOUR MAGIC 
+  if(!req.params.id){
+    next({ message: "account not found", status: 404 })
+  }else {
+    next();
+  }
 }
 
 exports.checkAccountId = async (req, res, next) => {
   // DO YOUR MAGIC
+  const {id} = req.params;
+  try {
+    const account = await accounts.getById(id);
+    if(account){
+      req.account = account;
+      next();
+    } else {
+
+    }
+  }
+  catch(error){
+
+  }
+
 }
--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,15 +1,33 @@
+const express = require('express')
+const accountModel = require('./accounts-model.js')
+
 const router = require('express').Router()
 
 router.get('/', async (req, res, next) => {
   // DO YOUR MAGIC
+  accountModel.get()
+  .then(account =>{
+    res.status(200).json(account);
+  })
+  .catch(error => {
+    res.status(500).json({ message: 'Error retrieving the Accounts.'});
+  });  
 })
 
 router.get('/:id', (req, res, next) => {
   // DO YOUR MAGIC
+  accountModel.getById(res.params.id)
+  .then(account => {
+    res.status(200).json(project)
+  })
+  .catch(error => {
+    res.status(500).json({ message: 'Error retrieving the Account Id.'})
+  })
 })
 
 router.post('/', (req, res, next) => {
   // DO YOUR MAGIC
+  
 })
 
 router.put('/:id', (req, res, next) => {
